b93accc913f5e4925cfa67b4a35ca6d7421bcdd1
Karan <krngrvr09@gmail.com>
2020-07-13 20:38:31 +0530

    Added Java code to call function from C file

--- app/src/main/java/com/example/ndktest2/MainActivity.java
+++ app/src/main/java/com/example/ndktest2/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.ndktest2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public native String  stringFromJNI();
+    static {
+        System.loadLibrary("test_file");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(),
+                stringFromJNI(),
+                Toast.LENGTH_SHORT);
+
+        toast.show();
+
     }
 }
