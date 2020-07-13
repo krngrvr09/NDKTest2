38c0e8499ba7654d3eadddb7971e754a4c06b84c
Karan <krngrvr09@gmail.com>
2020-07-13 21:35:28 +0530

    Added extern C for only cpp files

--- app/src/main/cpp/test_file.c
+++ app/src/main/cpp/test_file.c
@@ -1,6 +1,14 @@
 #include <jni.h>
 
-extern "C" JNIEXPORT jstring JNICALL
+#ifdef __cplusplus
+extern "C" {
+#endif
+    JNIEXPORT jstring JNICALL
+
+
 Java_com_example_ndktest2_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     return env->NewStringUTF("Hello from JNI LIBS!");
 }
+#ifdef __cplusplus
+}
+#endif
